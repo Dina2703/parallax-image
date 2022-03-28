512b7dfedf159d4aac5d8e2f29b9d563d58e15e0
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-28 12:46:30 -0400

    setting a fixed circle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import "./styles.css";
+
 function App() {
   return (
     <div>
-      <h2>Hello</h2>
+      <img src="/back.jpg" alt="background" />
+      <div style={styles.orb}></div>
     </div>
   );
 }
+
+const styles = {
+  orb: {
+    width: "300px",
+    height: "300px",
+    borderRadius: "50%",
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "green",
+  },
+};
 
 export default App;
