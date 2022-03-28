f8e008671176d605aea3e00e88ab5ba0681b8ff7
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-28 13:55:26 -0400

    setting reverse-direction and render-props examples

--- src/App.js
+++ src/App.js
@@ -18,10 +18,41 @@ function App() {
           <div style={inlineStyle}>HTML inside the parallax</div>
         </div>
       </Parallax>
-      <div style={{ textAlign: "center" }}>|||</div>
+      <div style={{ textAlign: "center", height: "50px" }}>|||</div>
       <Parallax bgImage="/back3.jpg" blur={{ min: -1, max: 2.5 }}>
         <div style={{ height: "700px" }}>
           <div style={inlineStyle}>Dynamic blur</div>
+        </div>
+      </Parallax>
+      <div style={{ textAlign: "center", height: "50px" }}>|||</div>
+      <Parallax bgImage="/back4.jpg" strength={-500}>
+        <div style={{ height: "700px" }}>
+          <div style={inlineStyle}>Reverse direction</div>
+        </div>
+      </Parallax>
+      <div style={{ textAlign: "center", height: "50px" }}>|||</div>
+      <Parallax
+        bgImage="/back5.jpg"
+        strength={-500}
+        renderLayer={(precentage) => (
+          <div
+            style={{
+              position: "absolute",
+              width: "100px",
+              height: "100px",
+              top: "50%",
+              left: "50%",
+              transform: `translate(-50%, -50%) scale(${precentage * 3})`,
+              background: `rgba(245, 40, 145, ${precentage * 0.5})`,
+              borderRadius: "50%",
+            }}
+          >
+            {console.log(precentage)}
+          </div>
+        )}
+      >
+        <div style={{ height: "700px" }}>
+          <div style={inlineStyle}>Render props</div>
         </div>
       </Parallax>
       <div style={{ height: "100vh" }}></div>
